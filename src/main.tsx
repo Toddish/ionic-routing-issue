38a827710a73299ddc8d916446bf1a462289e77a
Todd Francis <todd@toddish.co.uk>
2024-06-17 09:40:20 +0100

    Add in main changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import UserProvider from './contexts/user';
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </React.StrictMode>
 );
